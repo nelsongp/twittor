e840fe9993903896d9f8dd8a754230a2d067bf18
Nelson <nelsonsnake_@hotmail.com>
2021-02-22 16:07:38 -0600

    corrigiendo error

--- middlew/validoJWT.go
+++ middlew/validoJWT.go
@@ -1,13 +1,16 @@
 package middlew
 
-import "net/http"
+import (
+	"github.com/nelsongp/twittor/routers"
+	"net/http"
+)
 
 /*ValidoJWT permite validar el jwt que nos viene de la app */
 func ValidoJWT(next http.HandlerFunc) http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request){
 		_, _, _, err := routers.ProcesoToken(r.Header.Get("Authorization"))
 		if err != nil {
-			http.Error(w, "Error en el Token !" + err.Error()), http.StatusBadRequest
+			http.Error(w, "Error en el Token !" + err.Error(), http.StatusBadRequest)
 			return
 		}
 		next.ServeHTTP(w,r)
